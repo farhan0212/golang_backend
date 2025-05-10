e0acab82499480ff0aca541b7c653cf4123d6e84
Ubuntu <ubuntu@ip-172-31-31-139.ap-southeast-1.compute.internal>
2025-05-10 03:32:24 +0000

    first commit

--- models/user_models.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type User struct {
-	ID    uint   `json:"id" gorm:"primaryKey"`
-	Name  string `json:"name" gorm:"not null"`
-	Email string `json:"email" gorm:"unique;not null"`
-}
